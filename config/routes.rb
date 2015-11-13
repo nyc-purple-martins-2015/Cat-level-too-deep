85f02d0ca82a4e8d3d56ee0a5dc22ab35d44584d
Amy Liao <amyhliao88@gmail.com>
2015-11-12 16:09:06 -0800

    add rails routes to config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-
   resources :tags, only: [:index, :show]
   get '/tags' => 'tags#index'
   root 'questions#index'
   resources :questions
+  resources :users, only: :create
+  get 'register' => 'users#new', as: :register
+  get 'logout' => 'sessions#destroy', as: :logout
+  get 'login' => 'sessions#new', as: :login
+  post 'session_create' => 'sessions#create', as: :session_create
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
