ec2fb45d9f46e1b1d026d1c3c2a900d9e0f8deb8
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-12 21:33:42 -0500

    Add tags route, part of questions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :tags, only: [:index, :show] do
+    resources :questions
+   end
+  get '/tags' => 'tags#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
