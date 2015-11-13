aff5aa466445d6e8544add81129e9b4bea99e1d7
Amy Liao <amyhliao88@gmail.com>
2015-11-13 11:49:40 -0800

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username}!"
       redirect_to root_path
     else
