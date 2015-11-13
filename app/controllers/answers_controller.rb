ee6b820008311e60b512f8bbdc722e9509a8af55
Jon Star <email": null>
2015-11-12 19:10:32 -0500

    Add answer controller and create folders for eventual partial

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,21 @@
+class AnswersController < ApplicationController
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to("/questions/#{@question.id}")
+    else
+      flash.now[:notice] = "Response can't be blank."
+      render :"questions/show"
+    end
+  end
+
+  private
+
+  def convo_params
+    params.require(:answer).permit(:response).merge(user: current_user, question: @question)
+  end
+
+
+end
