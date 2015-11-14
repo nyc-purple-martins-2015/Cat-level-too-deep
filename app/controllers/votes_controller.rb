1dec5b3b04623ffc00e4b242cc8b25481deab767
Amy Liao & Lawson <pair+amyhliao.sonomar@devbootcamp.com>
2015-11-13 20:03:08 -0500

    Complete basic vote functionality with cat heads functionality implemented

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,11 @@
 class VotesController < ApplicationController
 
-  def create do
-    @vote = current_user.votes.new(vote_params)
-    if @vote.save
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save && @vote.votable_type == "question"
       redirect_to questions_path
+    elsif @vote.save && @vote.votable_type == "answer"
+      redirect_to question_path(id: Answer.find(@vote.votable_id).question.id)
     else
       render :index
     end
@@ -12,7 +14,7 @@ class VotesController < ApplicationController
   private
 
   def vote_params
-    params.require(:vote).permit(:up_vote, :votable, :user_id )
+    params.require(:vote).permit(:up_vote, :votable_id, :votable_type, :user_id )
   end
 
 end
