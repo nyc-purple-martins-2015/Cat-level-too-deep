9b816092b1d1862ba47ba8d76934c36a3fb9e32e
Amy Liao & Lawson <pair+amyhliao.sonomar@devbootcamp.com>
2015-11-13 20:15:27 -0500

    fix votable type to be capitalized

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,9 +2,9 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(vote_params)
-    if @vote.save && @vote.votable_type == "question"
+    if @vote.save && @vote.votable_type == "Question"
       redirect_to questions_path
-    elsif @vote.save && @vote.votable_type == "answer"
+    elsif @vote.save && @vote.votable_type == "Answer"
       redirect_to question_path(id: Answer.find(@vote.votable_id).question.id)
     else
       render :index
