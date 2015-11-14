bc8db61121ff2e161766d8be0c27cdc56d03c0fe
Amy Liao <amyhliao88@gmail.com>
2015-11-14 10:24:13 -0800

    make changes and clean up syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,17 @@ Rails.application.routes.draw do
   resources :users, only: :create
   resources :tags, only: [:index, :show]
   resource :session,  only: [:new, :create, :destroy]
-
   resource :votes, only: [:create]
-
   resource :comments, only: [:create]
-
   resources :questions, only: [:index, :show, :new, :create] do
     resources :answers, only: [:create]
   end
 
   root 'questions#index'
   get 'register' => 'users#new', as: :register
-
   get 'logout' => 'sessions#destroy', as: :logout
   get 'login' => 'sessions#new', as: :login
   post 'session_create' => 'sessions#create', as: :session_create
-
   get '/tags' => 'tags#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
