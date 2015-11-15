f005d662075bfcd01edf36f9607f0b86932284bd
Jon Star <email": null>
2015-11-14 22:09:06 -0500

    add comments testing

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -10,16 +10,27 @@ describe CommentsController  do
     stub_current_user(FactoryGirl.create(:user))
   end
 
-  context 'comment_params' do
-    it 'returns the correct params hash when comment_params is called' do
+  context '#create' do
+    it 'creates a new comment and assigns it to @comment' do
       post :create, comment: {content: comment_on_answer.content, commentable_id: comment_on_answer.commentable_id, commentable_type: comment_on_answer.commentable_type, user_id: comment_on_answer.user_id}
       expect(assigns(:comment)).to be_a(Comment)
     end
-    it 'redirects to the question path if a comment is saved on a question' do
+    it 'creates a new comment and assigns it to @comment' do
+      post :create, comment: {content: comment_on_question.content, commentable_id: comment_on_question.commentable_id, commentable_type: comment_on_question.commentable_type, user_id: comment_on_question.user_id}
+      expect(assigns(:comment)).to be_a(Comment)
+    end
+    it 'redirects to the question path if a comment on a question is saved' do
       post :create, comment: {content: comment_on_question.content, commentable_id: comment_on_question.commentable_id, commentable_type: comment_on_question.commentable_type, user_id: comment_on_question.user_id}
       expect(response).to redirect_to questions_path
     end
+    it 'redirects to the question path if a comment on an answer is saved' do
+      post :create, comment: {content: comment_on_answer.content, commentable_id: comment_on_answer.commentable_id, commentable_type: comment_on_answer.commentable_type, user_id: comment_on_answer.user_id}
+      expect(response).to redirect_to question_path(comment_on_answer.commentable.question.id)
+    end
+    it 'redirects to root page if the comment is not saved and alerts the user' do
+      post :create, comment: {content: "", commentable_id: 1, commentable_type: "", user_id: 1}
+      expect(flash[:alert]).to eq("We are sorry but your comment failed to save")
+      expect(response).to redirect_to(root_path)
+    end
   end
-
 end
-      # { content: comment_on_answer.content, commentable_id: comment_on_answer.commentable_id, commentable_type: comment_on_answer.commentable_type, user_id: comment_on_answer.user_id }
