4df2f75149b7cf5041d3914ff286b148d8e157c5
Amy Liao <amyhliao88@gmail.com>
2015-11-14 16:30:18 -0800

    add error messages for login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
 
   def create
     @questions = Question.all
+    @comment = Comment.new
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
