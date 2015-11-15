30f5ef0fb1d65e554396d9b2e566b3cd3fad06e9
Amy Liao & Lawson <pair+amyhliao.sonomar@devbootcamp.com>
2015-11-15 16:19:52 -0500

    add sorting functionaility

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,7 +16,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :commentable_id, :commentable_type, :user_id )
+    params.require(:comment).permit(:content, :commentable_id, :commentable_type).merge(user_id: current_user.id)
   end
 
 end
