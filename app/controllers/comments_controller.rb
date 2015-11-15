32c442e89e88e9c62c6992dc531d317d7fae5832
Amy Liao <amyhliao88@gmail.com>
2015-11-14 17:03:27 -0800

    add error handlers in comments controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+    @questions = Question.all
     @comment = Comment.new(comment_params)
     if @comment.save && @comment.commentable_type == "Question"
       redirect_to questions_path
     elsif @comment.save && @comment.commentable_type == "Answer"
       redirect_to question_path(id: Answer.find(@comment.commentable_id).question.id)
     else
-      #add error message
-      render :index
+      flash.now[:alert] = "Comment can't be blank."
+      render :"questions/index"
     end
   end
 
