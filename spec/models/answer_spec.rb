c36736965af2d9d1a9f9742f7474e3e3ab7c9455
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-15 22:28:25 -0500

    Add answer model tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "is invalid without a title" do
+      FactoryGirl.build(:answer, user: nil).should_not be_valid
+    end
+  it "is invalid without a query" do
+      FactoryGirl.build(:answer, question: nil).should_not be_valid
+    end
+  it "is invalid without an author" do
+      FactoryGirl.build(:answer, response: nil).should_not be_valid
+    end
+end
+
+
 end
